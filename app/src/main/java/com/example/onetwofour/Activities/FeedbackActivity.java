da2bb4e04d610d31de3551c36a575fc02ea591f9
win100 <lungthilinh2895>
2021-04-07 10:39:41 +0700

    processing

--- app/src/main/java/com/example/onetwofour/Activities/FeedbackActivity.java
+++ app/src/main/java/com/example/onetwofour/Activities/FeedbackActivity.java
@@ -3,14 +3,28 @@ package com.example.onetwofour.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 import com.example.onetwofour.R;
 
 public class FeedbackActivity extends AppCompatActivity {
-
+    RatingBar ratingBar;
+    Button btn;
+    EditText edt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        anhxa();
+        ratingBar.setNumStars(5);
+    }
+
+    private void anhxa() {
+        ratingBar = findViewById(R.id.ratingBar);
+        btn = findViewById(R.id.btn_publish_feedback);
+        edt = findViewById(R.id.edt_feedback);
     }
 }
